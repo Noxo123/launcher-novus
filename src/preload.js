cf14f150d06b566e2cca38633d646f9cfb756a0a
Noxo123 <138624794+Noxo123@users.noreply.github.com>
2026-08-30 20:19:18 +0200

    Expose launcher controls to Novus UI

--- src/preload.js
+++ src/preload.js
@@ -3,11 +3,13 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('novus', {
   getInfo: () => ipcRenderer.invoke('get-info'),
   install: () => ipcRenderer.invoke('install'),
-  launch: () => ipcRenderer.invoke('launch'),
+  launch: server => ipcRenderer.invoke('launch', server || null),
+  openGameDir: () => ipcRenderer.invoke('open-game-dir'),
   onStatus: callback => ipcRenderer.on('status', (_event, data) => callback(data)),
   onProgress: callback => ipcRenderer.on('progress', (_event, data) => callback(data)),
   onError: callback => ipcRenderer.on('error', (_event, data) => callback(data)),
   onInstalled: callback => ipcRenderer.on('installed', (_event, data) => callback(data)),
   onGameExit: callback => ipcRenderer.on('game-exit', (_event, data) => callback(data)),
-  onGameError: callback => ipcRenderer.on('game-error', (_event, data) => callback(data))
+  onGameError: callback => ipcRenderer.on('game-error', (_event, data) => callback(data)),
+  onGameLog: callback => ipcRenderer.on('game-log', (_event, data) => callback(data))
 });
